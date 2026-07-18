874e642365c5f3eb14678d9c92fd3e2cd166e810
mateo <mateofeit@gmail.com>
2026-07-18 16:07:00 -0600

    cyber janitor going to remove dds wholebody and replay action sources so that i can just modify base dds for what i want

--- action_provider/create_action_provider.py
+++ action_provider/create_action_provider.py
@@ -1,9 +1,9 @@
 from action_provider.action_provider_dds import DDSActionProvider
 
 
-from action_provider.action_provider_replay import FileActionProviderReplay
-
-from action_provider.action_provider_wh_dds import DDSRLActionProvider
+#from action_provider.action_provider_replay import FileActionProviderReplay
+#
+#from action_provider.action_provider_wh_dds import DDSRLActionProvider
 from pathlib import Path
 
 
@@ -14,13 +14,13 @@ def create_action_provider(env,args):
             env=env,
             args_cli=args
         )
-    elif args.action_source == "dds_wholebody":
-        return DDSRLActionProvider(
-            env=env,
-            args_cli=args
-        )
-    elif args.action_source == "replay":
-        return FileActionProviderReplay(env=env,args_cli=args)
+    #elif args.action_source == "dds_wholebody":
+    #    return DDSRLActionProvider(
+    #        env=env,
+    #        args_cli=args
+    #    )
+    #elif args.action_source == "replay":
+    #    return FileActionProviderReplay(env=env,args_cli=args)
     else:
         print(f"unknown action source: {args.action_source}")
         return None
